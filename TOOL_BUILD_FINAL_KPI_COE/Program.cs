6294b271f5d87bf1d95a3db7007bfe3731223617
theanhnguyen <theanhnguyen@vnpt.vn>
2025-06-24 16:28:30 +0700

    Add project files.

---
+++ TOOL_BUILD_FINAL_KPI_COE/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TOOL_BUILD_FINAL_KPI_COE
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
